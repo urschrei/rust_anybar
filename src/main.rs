dbe06ca5036afa28e6e13405bbd81535dea7fa89
Stephan Hügel <urschrei@gmail.com>
2015-06-03 12:51:45 +0100

    First pass at working program

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 use std::thread;
 use std::net;
+use std::env;
 
 fn socket(listen_on: net::SocketAddr) -> net::UdpSocket {
     let attempt = net::UdpSocket::bind(listen_on);
     let mut socket;
     match attempt {
         Ok(sock) => {
-            println!("Bound socket to {}", listen_on);
             socket = sock;
         },
     Err(err) => panic!("Could not bind: {}", err)
@@ -16,13 +16,11 @@ fn socket(listen_on: net::SocketAddr) -> net::UdpSocket {
 
 fn read_message(socket: net::UdpSocket) -> Vec<u8> {
     let mut buf: [u8; 1] = [0; 1];
-    println!("Reading data");
     let result = socket.recv_from(&mut buf);
     drop(socket);
     let mut data;
     match result {
         Ok((amt, src)) => {
-        println!("Received data from {}", src);
         data = Vec::from(&buf[0..amt]);
         },
     Err(err) => panic!("Read error: {}", err)
@@ -32,13 +30,12 @@ fn read_message(socket: net::UdpSocket) -> Vec<u8> {
 
 pub fn send_message(send_addr: net::SocketAddr, target: net::SocketAddr, data: Vec<u8>) {
     let socket = socket(send_addr);
-    println!("Sending data");
     let result = socket.send_to(&data, target);
     drop(socket);
-    match result {
-        Ok(amt) => println!("Sent {} bytes", amt),
-        Err(err) => panic!("Write error: {}", err)
-    }
+    // match result {
+    //     Ok(amt) => println!("Sent {} bytes", amt),
+    //     Err(err) => panic!("Write error: {}", err)
+    // }
 }
 
 pub fn listen(listen_on: net::SocketAddr) -> thread::JoinHandle<Vec<u8>> {
@@ -78,12 +75,50 @@ mod test {
 }
 
 fn main() {
+    // get command-line input
+
+    // this waits on a prompt, so
+    // http://stackoverflow.com/a/27973038/416626
+    // use std::io;
+    // use std::io::prelude::*;
+    // let stdin = io::stdin();
+    // let inp = stdin
+    //     .lock()
+    //     .lines()
+    //     .next()
+    //     .unwrap()
+    //     .unwrap()
+    //     .trim()
+    //     .to_string();
+
+    // Strings do not live for the entire life of your program
+    // http://stackoverflow.com/a/23977218/416626
+    let args: Vec<String> = env::args().collect();
+    let arg = args[1].clone();
+    let intermediate: &str = &*arg;
+
+    // match input or bail out horribly
+    let to_send = match intermediate {
+        "white" => "white".to_string(),
+        "red" => "red".to_string(),
+        "orange" => "orange".to_string(),
+        "yellow" => "yellow".to_string(),
+        "green" => "green".to_string(),
+        "cyan" => "cyan".to_string(),
+        "blue" => "blue".to_string(),
+        "purple" => "purple".to_string(),
+        "black" => "black".to_string(),
+        "?" => "question".to_string(),
+        "!" => "exclamation".to_string(),
+        "quit" => "quit".to_string(),
+        _ => panic!("Unknown option!")
+    };
+
+    let mut message: Vec<u8> = Vec::new();
+    message.extend(to_send.as_bytes().iter().cloned());
     let ip = net::Ipv4Addr::new(127, 0, 0, 1);
     let listen_addr = net::SocketAddrV4::new(ip, 1738);
     let send_addr = net::SocketAddrV4::new(ip, 0);
-    let s = "red".to_string();
-    let mut message: Vec<u8> = Vec::new();
-    message.extend(s.as_bytes().iter().cloned());
     send_message(
         net::SocketAddr::V4(send_addr),
         net::SocketAddr::V4(listen_addr),
