27247cb8dbb7e83ee0957f8180671044014ed92a
Stephan Hügel <urschrei@gmail.com>
2015-06-09 12:19:15 +0100

    Removing unused UDP functions

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 // UDP stuff adapted from http://illegalargumentexception.blogspot.co.uk/2015/05/rust-send-and-receive-on-localhost-with.html
-use std::thread;
 use std::net;
 use std::env;
 use std::convert::AsRef;
@@ -18,60 +17,10 @@ fn socket(listen_on: net::SocketAddr) -> net::UdpSocket {
     socket
 }
 
-fn read_message(socket: net::UdpSocket) -> Vec<u8> {
-    let mut buf: [u8; 1] = [0; 1];
-    let result = socket.recv_from(&mut buf);
-    drop(socket);
-    let mut data;
-    match result {
-        Ok((amt, _)) => {
-        data = Vec::from(&buf[0..amt]);
-        },
-    Err(err) => panic!("Read error: {}", err)
-    }
-    data
-}
-
 pub fn send_message(send_addr: net::SocketAddr, target: net::SocketAddr, data: Vec<u8>) {
     let socket = socket(send_addr);
     let _ = socket.send_to(&data, target);
     drop(socket);
-}
-
-pub fn listen(listen_on: net::SocketAddr) -> thread::JoinHandle<Vec<u8>> {
-    let socket = socket(listen_on);
-    let handle = thread::spawn(move || {
-        read_message(socket)
-    });
-    handle
-}
-
-#[cfg(test)]
-mod test {
-    use std::net;
-    use std::thread;
-    use super::*;
-
-    #[test]
-    fn test_udp() {
-        println!("UDP");
-        let ip = net::Ipv4Addr::new(127, 0, 0, 1);
-        let listen_addr = net::SocketAddrV4::new(ip, 8888);
-        let send_addr = net::SocketAddrV4::new(ip, 8889);
-        let future = listen(net::SocketAddr::V4(listen_addr));
-        let message: Vec<u8> = vec![10];
-        // give the thread 3s to open the socket
-        thread::sleep_ms(3000);
-        send_message(
-            net::SocketAddr::V4(send_addr),
-            net::SocketAddr::V4(listen_addr),
-            message);
-        println!("Waiting");
-        let received = future.join().unwrap();
-        println!("Got {} bytes", received.len());
-        assert_eq!(1, received.len());
-        assert_eq!(10, received[0]);
-    }
 }
 
 fn convert(inp: &str) -> String {
@@ -90,7 +39,6 @@ fn print_usage(program: &str, opts: Options, ecode: i32) {
     print!("{}", opts.usage(&brief));
     std::process::exit(ecode);
 }
-
 
 fn main() {
     let mut opts = Options::new();
