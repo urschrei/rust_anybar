fa324d0829a2c6dddb3582c399d424e9d2903ea3
Stephan Hügel <stephan.hugel.12@ucl.ac.uk>
2015-12-18 18:15:49 +0000

    Formatting changes, and removing unneeded mut from socket

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,12 @@ use getopts::Options;
 
 fn socket(listen_on: net::SocketAddr) -> net::UdpSocket {
     let attempt = net::UdpSocket::bind(listen_on);
-    let mut socket;
+    let socket;
     match attempt {
-        Ok(sock) => { socket = sock; },
-        Err(err) => panic!("Could not bind: {}", err)
+        Ok(sock) => {
+            socket = sock;
+        }
+        Err(err) => panic!("Could not bind: {}", err),
     }
     socket
 }
@@ -29,10 +31,9 @@ fn convert(inp: String) -> String {
     // Strings do not live for the entire life of your program
     // http://stackoverflow.com/a/23977218/416626
     let intermediate: &str = &*inp;
-    let outp = intermediate
-        .chars()
-        .flat_map(char::to_lowercase)
-        .collect::<String>();
+    let outp = intermediate.chars()
+                           .flat_map(char::to_lowercase)
+                           .collect::<String>();
     outp
 }
 
@@ -47,10 +48,13 @@ fn main() {
     // get command-line input
     let args: Vec<String> = env::args().collect();
     let program = args[0].clone();
-    opts.optopt("p", "port", "Set destination UDP port. Input must be 0 – 65535", "PORT");
+    opts.optopt("p",
+                "port",
+                "Set destination UDP port. Input must be 0 – 65535",
+                "PORT");
     let matches = match opts.parse(&args[1..]) {
-        Ok(m) => {m}
-        Err(f) => { panic!(f.to_string()) }
+        Ok(m) => m,
+        Err(f) => panic!(f.to_string()),
     };
     // Get port from the option, or specify the default
     let port = matches.opt_str("p").unwrap_or("1738".to_string());
@@ -82,22 +86,19 @@ fn main() {
             return;
         }
     };
-    // blam our control message into a vector 
+    // blam our control message into a vector
     let mut message: Vec<u8> = Vec::new();
-    message.extend(to_send
-        .as_bytes()
-        .iter()
-        .cloned()
-    );
+    message.extend(to_send.as_bytes()
+                          .iter()
+                          .cloned());
     // bind to the correct UDP port
     let ip = net::Ipv4Addr::new(127, 0, 0, 1);
     // parse() gives us Result which we need to unwrap
-    // it's safe to unwrap here, cos we already checked success 
+    // it's safe to unwrap here, cos we already checked success
     let listen_addr = net::SocketAddrV4::new(ip, numeric_port.unwrap());
     let send_addr = net::SocketAddrV4::new(ip, 0);
     // and send our message
-    send_message(
-        net::SocketAddr::V4(send_addr),
-        net::SocketAddr::V4(listen_addr),
-        message);
+    send_message(net::SocketAddr::V4(send_addr),
+                 net::SocketAddr::V4(listen_addr),
+                 message);
 }
