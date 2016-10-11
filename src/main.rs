e485ff424cb6e1cfbe5ff842b2a51395ad42494e
Stephan Hügel <stephan.hugel.12@ucl.ac.uk>
2016-10-11 17:56:14 +0100

    Reduce allocations
    There's no need to:
    - take ownership of the matches COMMAND entry
    - clone it into a new vector of bytes
    
    because send_to expects borrowed content anyway, we can:
    - unwrap COMMAND, and represent it as lowercase
    - pass that value to send_message as a slice of bytes
    
    No additional allocations!

--- src/main.rs
+++ src/main.rs
@@ -16,9 +16,9 @@ fn socket(listen_on: net::SocketAddr) -> net::UdpSocket {
     socket
 }
 
-pub fn send_message(send_addr: net::SocketAddr, target: net::SocketAddr, data: Vec<u8>) {
+pub fn send_message(send_addr: net::SocketAddr, target: net::SocketAddr, data: &[u8]) {
     let socket = socket(send_addr);
-    let _ = socket.send_to(&data, target);
+    let _ = socket.send_to(data, target);
     drop(socket);
 }
 
@@ -27,30 +27,26 @@ fn main() {
     let command_vals = ["white", "red", "orange", "yellow", "green", "cyan", "blue", "purple",
                         "black", "?", "!", "quit"];
     let matches = App::new("rust_anybar")
-        .version(&crate_version!()[..])
-        .author("Stephan Hügel <urschrei@gmail.com>")
-        .about("A Rust command-line client for Anybar")
-        .args_from_usage("-p --port=[PORT] 'Set destination UDP port. Input must be 0 – 65535, \
-                          and defaults to 17388")
-        .arg(Arg::with_name("COMMAND")
-            .help("The command you wish to send to Anybar")
-            .index(1)
-            .possible_values(&command_vals)
-            .required(true))
-        .get_matches();
-    let numeric_port = value_t!(matches.value_of("PORT"), u16).unwrap_or(1738);
-    let to_send = matches.value_of("COMMAND").unwrap().to_owned().to_lowercase();
-    // blam our control message into a vector
-    let mut message: Vec<u8> = Vec::new();
-    message.extend(to_send.as_bytes()
-        .iter()
-        .cloned());
+                      .version(&crate_version!()[..])
+                      .author("Stephan Hügel <urschrei@gmail.com>")
+                      .about("A Rust command-line client for Anybar")
+                      .args_from_usage("-p --port=[PORT] 'Set destination UDP port. Input must \
+                                        be 0 – 65535, and defaults to 17388")
+                      .arg(Arg::with_name("COMMAND")
+                               .help("The command you wish to send to Anybar")
+                               .index(1)
+                               .possible_values(&command_vals)
+                               .required(true))
+                      .get_matches();
     // bind to the correct UDP port
+    let numeric_port = value_t!(matches.value_of("PORT"), u16).unwrap_or(1738);
+    // it's safe to unwrap here, cos we already checked success
+    let to_send = matches.value_of("COMMAND").unwrap().to_lowercase();
     let ip = net::Ipv4Addr::new(127, 0, 0, 1);
     let listen_addr = net::SocketAddrV4::new(ip, numeric_port);
     let send_addr = net::SocketAddrV4::new(ip, 0);
     // and send our message
     send_message(net::SocketAddr::V4(send_addr),
                  net::SocketAddr::V4(listen_addr),
-                 message);
+                 to_send.as_bytes());
 }
