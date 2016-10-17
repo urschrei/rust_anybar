8a5a22f6bc427cfd73583bbbac691898d4bb3372
Stephan Hügel <stephan.hugel.12@ucl.ac.uk>
2016-10-17 15:55:04 +0100

    Spin up a thread to send the message

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 // UDP stuff adapted from http://illegalargumentexception.blogspot.co.uk/2015/05/rust-send-and-receive-on-localhost-with.html
 use std::net::{SocketAddr, SocketAddrV4, Ipv4Addr, UdpSocket};
+use std::process::exit;
+use std::thread;
 #[macro_use]
 extern crate clap;
 use clap::{Arg, App};
@@ -45,15 +47,18 @@ fn main() {
             .possible_values(&command_vals)
             .required(true))
         .get_matches();
-    // bind to the correct UDP port
-    let numeric_port = value_t!(command_params.value_of("PORT"), u16).unwrap_or(1738);
-    // it's safe to unwrap here, cos we already checked success
-    let to_send = command_params.value_of("COMMAND").unwrap();
-    let ip = Ipv4Addr::new(127, 0, 0, 1);
-    let listen_addr = SocketAddrV4::new(ip, numeric_port);
-    let send_addr = SocketAddrV4::new(ip, 0);
-    // and send our message
-    send_message(SocketAddr::V4(send_addr),
-                 SocketAddr::V4(listen_addr),
-                 to_send.as_bytes());
+    let handle = thread::spawn(move || {
+        // bind to the correct UDP port
+        let numeric_port = value_t!(command_params.value_of("PORT"), u16).unwrap_or(1738);
+        // it's safe to unwrap here, cos we already checked success
+        let to_send = command_params.value_of("COMMAND").unwrap();
+        let ip = Ipv4Addr::new(127, 0, 0, 1);
+        let listen_addr = SocketAddrV4::new(ip, numeric_port);
+        let send_addr = SocketAddrV4::new(ip, 0);
+        // and send our message
+        send_message(SocketAddr::V4(send_addr),
+                     SocketAddr::V4(listen_addr),
+                     to_send.as_bytes());
+    });
+    let _ = handle.join();
 }
