96801268d61850d4f3f79b64eca7bfb73872278c
Stephan Hügel <shugel@tcd.ie>
2024-12-28 18:56:46 +0000

    Update Clap to v4.x

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 // UDP stuff adapted from http://illegalargumentexception.blogspot.co.uk/2015/05/rust-send-and-receive-on-localhost-with.html
-use clap::{arg, crate_version, App, Arg};
+use clap::{crate_version, value_parser, Arg, Command};
 use std::net::{Ipv4Addr, SocketAddr, SocketAddrV4, UdpSocket};
 
 fn socket(listen_on: SocketAddr) -> UdpSocket {
@@ -32,29 +32,38 @@ fn main() {
         "hollow",
         "quit",
     ];
-    let command_params = App::new("rust_anybar")
+    let command_params = Command::new("rust_anybar")
         .version(crate_version!())
         .author("Stephan Hügel <urschrei@gmail.com>")
         .about("A Rust command-line client for Anybar 0.2.3")
         .arg(
-            arg!(-p --port <PORT> "Set destination UDP port. Input must be 0 – 65535")
-                .default_value("1738")
-                .required(false),
+            Arg::new("PORT")
+                .short('p')
+                .long("port")
+                .help("Set destination UDP port. Input must be 0–65535")
+                .value_parser(value_parser!(u16).range(0..=65535))
+                .default_value("1738"),
         )
         .arg(
             Arg::new("COMMAND")
                 .help("The command you wish to send to Anybar")
                 .index(1)
-                .possible_values(command_vals)
-                .required(true),
+                .required(true)
+                // Use value_parser to supply valid possible values
+                .value_parser(command_vals),
         )
         .get_matches();
     // bind to the correct UDP port
-    let numeric_port: u16 = command_params.value_of_t("PORT").unwrap();
+    let numeric_port: u16 = *command_params
+        .get_one::<u16>("PORT")
+        .expect("Failed to get port");
+    let command = command_params
+        .get_one::<String>("COMMAND")
+        .expect("COMMAND argument missing");
     // and send our message
     send_message(
         SocketAddr::V4(SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1), 0)),
         SocketAddr::V4(SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1), numeric_port)),
-        command_params.value_of("COMMAND").unwrap().as_bytes(),
+        command.as_bytes(),
     );
 }
